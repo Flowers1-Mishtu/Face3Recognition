c1b6b855b707bf937ac6b39675548b7ee5582102
Flowers1-Mishtu <87982268+Flowers1-Mishtu@users.noreply.github.com>
2023-02-08 19:42:52 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_pic()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Cd0tH2L6m/",modelLoaded);
+
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+
+function check(){
+img = document.getElementById('captured_image');
+classifier.classify(img, gotresult);
+}
+
+function gotresult(error, results){
+    if (error){
+        console.error(error);
+} else {
+console.log(results);
+document.getElementById("result_person_name").innerHTML ="Object : " + results[0].label;
+document.getElementById("result_person_accuracy").innerHTML = "Accuracy : " + results[0].confidence.toFixed(3);
+
+}
+}
